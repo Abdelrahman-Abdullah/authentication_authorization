552bb5dfbffd4ffd07b0a4d85f647ecd50a37c93
abdelra7man <abdelra7manabdullah@gmail.com>
2023-12-26 19:14:43 +0200

    update forget password form adding action

--- resources/views/front/auth/forget-password.blade.php
+++ resources/views/front/auth/forget-password.blade.php
@@ -33,8 +33,9 @@
                 <div class="card-body">
                     @include('front.partials.logo')
                     <h4 class="mb-2">Forgot Password? 🔒</h4>
-                    <p class="mb-4">Enter your email and we'll send you instructions to reset your password</p>
-                    <form id="formAuthentication" class="mb-3" action="index.html" method="POST">
+                    <p class="mb-4">Enter your email, and we'll send you instructions to reset your password</p>
+                    <form id="formAuthentication" class="mb-3" action="{{route('password.email')}}" method="POST">
+                        @csrf
                         <div class="mb-3">
                             <label for="email" class="form-label">Email</label>
                             <input
@@ -44,12 +45,13 @@
                                 name="email"
                                 placeholder="Enter your email"
                                 autofocus
+                                :value="old('email')"
                             />
                         </div>
                         <button class="btn btn-primary d-grid w-100">Send Reset Link</button>
                     </form>
                     <div class="text-center">
-                        <a href="auth-login-basic.html" class="d-flex align-items-center justify-content-center">
+                        <a href="{{route('login')}}" class="d-flex align-items-center justify-content-center">
                             <i class="bx bx-chevron-left scaleX-n1-rtl bx-sm"></i>
                             Back to login
                         </a>
